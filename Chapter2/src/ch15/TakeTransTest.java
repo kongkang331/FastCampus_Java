500c95282eb44c7bcb073ac4adb2fa78bf917d9d
kongkang331 <lsb1896@naver.com>
2022-07-13 02:55:45 +0900

    create chapter2/src/ch15/

---
+++ Chapter2/src/ch15/TakeTransTest.java
@@ -0,0 +1,17 @@
+package ch15;
+
+public class TakeTransTest {
+
+	public static void main(String[] args) {
+		
+		Person person = new Person("Edward", 20000);
+		Taxi taxi = new Taxi("잘나간다 운수");
+		
+		person.takeTaxi(taxi);
+		
+		person.showPersonInfo();
+		taxi.showTaxiInfo();
+		
+	}
+	
+}
