23ed833665fdd76017825beb8a66366b0428c4ab
kongkang331 <lsb1896@naver.com>
2022-07-21 01:22:02 +0900

    Update Chapter6/src/ch16/

--- Chapter6/src/ch16/FileCopyTest.java
+++ Chapter6/src/ch16/FileCopyTest.java
@@ -2,16 +2,19 @@ package ch16;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class FileCopyTest {
 
 	public static void main(String[] args) {
 
 		long millisecond = 0;
-		
+		// 보조 스트림은 계속 감쌀 수 있고, 최종은 기반 스트림 
 		try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream("a.zip"));
 			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("copy.zip"))) {
 				
@@ -30,7 +33,10 @@ public class FileCopyTest {
 		}
 		
 		System.out.println(millisecond + "초 소요되었습니다.");
-
+		
+//		Socket socket = new Socket();
+//		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//		br.readLine();
 	}
 
 }
